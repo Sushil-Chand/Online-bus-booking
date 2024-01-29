9fc51359fe9ffd492af2a5c695ec33b7e7d4eb78
Sushil Chand <sushilchand662@gmail.com>
2024-01-29 23:54:55 +0545

    change dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,6 +1,23 @@
-{{-- resources/views/admin/dashboard.blade.php --}}
-
-{{-- @extends('layouts.admin') Assuming you have a master layout for admin --}}
-{{-- @extends('layouts.admin') --}}
-
-<h1>Welcome you dashboard</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <!-- Add your stylesheets and scripts here -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    @include('layouts.navbar')
+    <div class="container-fluid">
+        <div class="row">
+            @include('layouts.sidebar')
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+                @yield('content')
+            </main>
+        </div>
+    </div>
+    <!-- Add your custom scripts here -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
