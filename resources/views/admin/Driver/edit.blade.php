d146bd6d45293a80967b2283188ecec81d4fba20
Sushil Chand <sushilchand662@gmail.com>
2024-02-13 23:58:24 +0545

    edit the driver data in index

--- resources/views/admin/Driver/edit.blade.php
+++ resources/views/admin/Driver/edit.blade.php
@@ -4,6 +4,10 @@
 @section('title', 'edit-driver')
 
 @section('content')
+<br>
+<br>
+<div class="content-wrapper">
+    <div class="content">
     <div class="container">
         <h2>Edit Driver</h2>
 
@@ -20,9 +24,34 @@
         <form action="{{ route('drivers.update', $driver->id) }}" method="POST">
             @csrf
             @method('PUT')
-            <div class="mb-3">
-                <label for="name" class="form-label">Name</label>
-                <input type="text" class="form-control" id="name" name="name" value="{{ $driver->name }}" required>
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" name="name" id="name" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+                <label for="gender">Gender:</label>
+                <select name="gender" id="gender" class="form-control" required>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="license_number">License Number:</label>
+                <input type="text" name="license_number" id="license_number" class="form-control" required>
+            </div>
+
+            <div class="form-group">
+                <label for="contact_number">Contact Number:</label>
+                <input type="text" name="contact_number" id="contact_number" class="form-control" required>
+            </div>
+
+          
+            <!-- Add more fields as needed -->
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Create Driver</button>
             </div>
             <!-- Add other fields here based on your model attributes -->
             <button type="submit" class="btn btn-primary">Update</button>
